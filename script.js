970a5474e64ab441f832f957a782558a0280412b
sabaly34 <sabsdurant7@gmail.com>
2024-12-14 17:45:18 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #f4f4f9;
+  color: #333;
+}
+
+header {
+  background-color: #4CAF50;
+  color: white; 
+  padding: 10px 0;
+  text-align: center;
+}
+
+footer {
+  text-align: center;
+  padding: 10px 0;
+  background-color: #222;
+  color: white;
+
+  alert("Bonjour ! Bienvenue sur mon site !");
+
